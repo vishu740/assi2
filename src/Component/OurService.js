c474cb6b2d9fcb7678a0bb8afedfeae8421793c8
VISHAL KUMAR <vk8886067@gmail.com>
2022-06-02 22:07:22 +0530

    change

--- src/Component/OurService.js
+++ src/Component/OurService.js
@@ -11,33 +11,33 @@ function OurService(){
     </div>
     <div className="container">
         <div className="row">
-        <div className="col-lg-3 col-md-6">
-        <div class="card cardcolor1">
+        <div className="col-lg-3 col-sm-6 col-md-6">
+        <div class="card cardcolor1 mb-3">
   <div class="medicalimg">
       <img src="Shape.png"></img>
       <p className="mt-4">Dantel Care</p>
   </div>
 </div>
         </div>
-        <div className="col-lg-3 col-md-6">
-        <div class="card cardcolor">
+        <div className="col-lg-3 col-sm-6 col-md-6">
+        <div class="card cardcolor mb-3">
   <div class="medicalimg">
       <img src="4.png"></img>
       <p className="mt-4">Pulmonary</p>
   </div>
 </div>
         </div>
-        <div className="col-lg-3 col-md-6">
-        <div class="card cardcolor1">
+        <div className="col-lg-3 col-sm-6 col-md-6">
+        <div class="card cardcolor1 mb-3">
   <div class="medicalimg">
       <img src="2.png"></img>
       <p className="mt-4">Neurological</p>
   </div>
 </div>
         </div>
-        <div className="col-lg-3 col-md-6">
+        <div className="col-lg-3 col-sm-6 col-md-6">
         <div class="card">
-  <div class="medicalimg cardcolor1">
+  <div class="medicalimg cardcolor1 mb-3">
       <img src="medical-file.png"></img>
       <p className="mt-4">Prediatrics</p>
   </div>
